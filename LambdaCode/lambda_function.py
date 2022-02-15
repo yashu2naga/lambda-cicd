888720193930522bb479117abc4b311fca8673e9
yashu2naga <93919022+yashu2naga@users.noreply.github.com>
2022-02-15 10:16:20 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -7,7 +7,7 @@ ses_client = boto3.client('ses',region_name='us-west-2')
 # if __name__ == '__main__':
 def lambda_handler(event, context):
     my_list=[('Abyaya', 'Hotta', 123), ('yasaswini', 'Kompala',333 ),
-             ('Sathyanarayana', 'Dathrika', 567), ('Praveen', 'Maroju', 221),
+             ('Sathyanarayana', 'Dathrika', 567), ('Praveen', 'Maroju', 121),
              ('Giridhar', 'Pochareddy', 151)]
     
     print(my_list)
